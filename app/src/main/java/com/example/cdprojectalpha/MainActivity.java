cd344a8eb32069905189ed37c2432ac56d06305b
JUMINJONG <wnalsals12@naver.com>
2019-10-20 19:18:13 +0900

    과제4 로그인 창

--- app/src/main/java/com/example/cdprojectalpha/MainActivity.java
+++ app/src/main/java/com/example/cdprojectalpha/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.cdprojectalpha;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static int connect = 0;
     Button b, b2;
-    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b = findViewById(R.id.button);
         b2 = findViewById(R.id.button2);
-        t = findViewById(R.id.textView14);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t.setText("시작");
+                login();
 //                Intent in = new Intent(getApplicationContext(), controller.class);
 //                startActivity(in);
             }
@@ -35,10 +35,62 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t.setText("옵션");
-//                Intent in = new Intent(getApplicationContext(), option.class);
-//                startActivity(in);
+                Intent in = new Intent(getApplicationContext(), option.class);
+                startActivity(in);
             }
         });
+    }
+
+    void login()
+    {
+        final EditText id = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("회원 로그인");
+        builder.setView(id);
+        builder.setMessage("아이디");
+        builder.setPositiveButton("다음",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(id.getText().toString().equals("user")){
+                            login_fin();
+                        }
+                        else
+                            Toast.makeText(getApplicationContext(), "존재하지 않는 아이디입니다!",Toast.LENGTH_LONG).show();
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
+    }
+
+    void login_fin()
+    {
+        final EditText pass = new EditText(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("회원 로그인");
+        builder.setMessage("비밀번호");
+        builder.setView(pass);
+        builder.setPositiveButton("로그인",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(pass.getText().toString().equals("1234567890"))
+                            Toast.makeText(getApplicationContext(), "회원입니다!",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(getApplicationContext(), "회원이 아닙니다!",Toast.LENGTH_LONG).show();
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        builder.show();
     }
 }
